71e50299bdab7556bc2c1aced0e569ad8e740fa1
Diego Fabbri <diegofabbri96@gmail.com>
2021-06-15 22:55:13 +0200

    Update Dati.java

--- Dati.java
+++ Dati.java
@@ -1,24 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Utility;
 
-/**
- *
- * @author diego
- */
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author diego
- */
 public class Dati {
 
     public static int Numero_giorni() {
